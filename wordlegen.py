2c06442c6d7a69798e293223869bd94753c512dd
Yusuf Ahmed <yusuf65820@gmail.com>
2025-10-03 17:46:21 +0100

    pregen all 5 letter real words

---
+++ wordlegen.py
@@ -0,0 +1,12 @@
+import itertools, string, enchant
+words= []
+dictionary=enchant.Dict("en_US") #wordle is from the NY times. NY is in the US ∴ en_US dict is used
+
+for combination in itertools.product(string.ascii_lowercase,repeat=5):
+    word = "".join(combination)
+    if dictionary.check(word):
+        words.append(word)
+with open("wordlegen.txt","w") as f:
+    for carrier in words:
+        f.write(carrier+"\n")
+    f.close()
