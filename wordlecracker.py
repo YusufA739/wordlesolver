2e4356950565a7f11d9d6dedee0d4ed27507fcec
Yusuf Ahmed <yusuf65820@gmail.com>
2026-01-18 19:50:48 +0000

    commenting, refactoring (moving imports) and testing

--- wordlecracker.py
+++ wordlecracker.py
@@ -1,4 +1,4 @@
-import itertools,string
+import itertools,string,enchant
 
 # Define all characters to use in the password
 chars = string.ascii_lowercase
@@ -21,7 +21,11 @@ with open("everywordleword.txt","r") as f:
 # preloadedCombos = itertools.combinations(data,len(data)) idk check it out later, never done it like this
 if len(preloadedCombos) > 0:
     for carrier in range(len(preloadedCombos)):
-        if preloadedCombos[carrier][:len(beginningPartial)] == beginningPartial: #list[index][string_index] is basically subStringing it (thanks, Chang-Qi)
+        if preloadedCombos[carrier][:len(beginningPartial)] == beginningPartial: #list[wordNo][string_index] is basically subStringing it (thanks, Chang-Qi)
+            #what it does is choose the word from the list so list1 = ["hello", "my", "name", "is", "Yusuf"] and basically first is choosing the word - understandable
+            #but as you can manip strings like lists in python (e.g: for carrier in "exampleWord": or word = "thisIsAWord" ... for carrier in word:)
+            #we can then use the [:n] notation where n is an integer representing the exclusive end range limit. As it is a string, not a list, the returned element fragment is...
+            #a string fragment, not a fragment of a list -> ["hello", "my"] vs "hel"
             possible_combos.append(preloadedCombos[carrier])
         else:
             pass
@@ -37,10 +41,10 @@ else: #brute force the list of all chars, then only save to memory those words t
             possible_combos.append(str(beginningPartial+candidate))
     #generate all possible combos
 
-yellow_letters=[] #if you have no words, don't just remove the character, delete the null string entry -> "" (do not just backspace the letters out)
-green_letters=['a','n','e']
-gray_letters=['f','r','o','t','i'] #confirmed gray letters, from previous guesses (DO NOT STORE DUPLICATES HERE)
-green_letter_positions=[2,3,4]
+yellow_letters=["c","a"] #if you have no words, don't just remove the character, delete the null string entry -> "" (do not just backspace the letters out)
+green_letters=[]
+gray_letters=["r","n","e"] #confirmed gray letters, from previous guesses (DO NOT STORE DUPLICATES HERE -> if a word has e in it, whether it be yellow or green, do not store the second e - for duplicates - in here)
+green_letter_positions=[]#NOTE: uses index 0 as first positon. Standard coding indexing means consistent
 aac=[] #stands for all actual combinations
 for word in possible_combos:
     match = False
@@ -57,28 +61,26 @@ for word in possible_combos:
         tempYellowsLetterCount = tempYellows.count(letter)
         for greenIndex in range(len(green_letters)):
             if green_letters[greenIndex] == letter and green_letter_positions[greenIndex] == letterIndex: #if the letter is in green_letters
-        # and the corresponding position pair index matches the current letterIndex, then we should increment
+                # and the corresponding position pair index matches the current letterIndex, then we should increment
                 greensPresent += 1
                 greenMatch = True
 
-        if not greenMatch and letter in yellow_letters and tempYellowsLetterCount < letterCount: #if letter is in yellow_letters, and we haven't reached yellow limit
+        if (not greenMatch) and (letter in yellow_letters) and (tempYellowsLetterCount < letterCount): #if letter is in yellow_letters, and we haven't reached yellow limit
             yellowsPresent += 1
         letterIndex += 1
 
     if greensPresent == len(green_letters) and yellowsPresent == len(yellow_letters):
-        match = True
-    if match:#match has been made redundant but a flag is more descriptive than a long boolean statement, so only for this reason will it not get deleted
         aac.append(word)
-print(aac)
 
-file1=open("aac.txt","w")
-for carrier in aac:
-    file1.write(carrier+"\n")
-file1.close()
+with open("aac.txt","w") as f:
+    for word in aac:
+        f.write(word+"\n")
+    f.close()
 
-import enchant
+
 dictionary=enchant.Dict("en_US") #wordle is from the NY times. NY is in the US ∴ en_US dict is used
 
 for carrier in aac:
     meaning = dictionary.check(carrier)
-    if meaning:print(carrier,"is a word")
+    if meaning:
+        print(carrier,"is a word")
